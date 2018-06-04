1691aaec1525b7da7b97f1c3bcd65110425ac0d5
chrispumita <christian.floppy@gmail.com>
2018-06-04 14:31:41 -0500

    ESCENIKA INDEX
    HEADER AND CONTENT MENU

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>ESCENIKA HOME</title>
+	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+	<link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+	<header id="header" class="header">
+		<div class="backImg">
+			<div class="menu">
+				<a href="#menu" id="btnStart" class="boton bTri btn1 btnInformes">INFORMES</a>
+			</div>
+			<div class="contLogo">
+				<div class="logoEsc">
+					<img class="logoImg" src="image/Escenika-LogoHome.png" alt="Logo de Escenika">
+				</div>
+				<div class="botonMenu">
+					<a href="#content" id="btnStart" class="boton bTri btn1">EMPEZAR</a>
+					<a href="" id="btnContact" class="boton bTri btn2">MODELOS</a>
+				</div>	
+			</div>
+		</div>
+	</header>
+<!-- /header -->
+<content id="content">
+	<div id="contentHeader">
+		<div class="contentMenu">
+			<div class="contLogoH">
+				<div class="logoH">
+					<img class="logoImgH" src="image/logohorizontal.png" alt="Logo de Escenika Horizontal">
+				</div>
+			</div>
+			<div class="contLogoMenus">
+				<div class="menus">
+					<a href="#"class="boton bTri btn3">EMPEZAR</a>
+					<a href="" class="boton bTri btn3">MODELOS</a>
+					<a href="#"class="boton bTri btn3">EMPEZAR</a>
+					<a href="" class="boton bTri btn3">MODELOS</a>
+				</div>
+			</div>
+		</div>
+		<div class="contPano">
+			
+			
+		</div>
+	</div>
+</content>
+<footer>
+	
+</footer>
+</body>
+</html>
