8aa5e780feecf34f7ecc5f4365b4a7e5b2715547
chrispumita <christian.floppy@gmail.com>
2018-06-06 13:19:01 -0500

    Last Day
    Actulizacion de Caractersiticas ESCENIKA

--- index.php
+++ index.php
@@ -7,49 +7,149 @@
 	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
 	<link rel="stylesheet" href="style/style.css">
 </head>
-<body>
-	<header id="header" class="header">
-		<div class="backImg">
-			<div class="menu">
-				<a href="#menu" id="btnStart" class="boton bTri btn1 btnInformes">INFORMES</a>
-			</div>
-			<div class="contLogo">
-				<div class="logoEsc">
-					<img class="logoImg" src="image/Escenika-LogoHome.png" alt="Logo de Escenika">
+	<body>
+		<header id="header" class="header">
+			<div class="backImg">
+				<div class="menu">
+					<a href="#menu" id="btnStart" class="boton bTri btn1 btnInformes">INFORMES</a>
 				</div>
-				<div class="botonMenu">
-					<a href="#content" id="btnStart" class="boton bTri btn1">EMPEZAR</a>
-					<a href="" id="btnContact" class="boton bTri btn2">MODELOS</a>
-				</div>	
-			</div>
-		</div>
-	</header>
-<!-- /header -->
-<content id="content">
-	<div id="contentHeader">
-		<div class="contentMenu">
-			<div class="contLogoH">
-				<div class="logoH">
-					<img class="logoImgH" src="image/logohorizontal.png" alt="Logo de Escenika Horizontal">
+				<div class="contLogo">
+					<div class="logoEsc">
+						<img class="logoImg" src="image/Escenika-LogoHome.png" alt="Logo de Escenika">
+					</div>
+					<div class="botonMenu">
+						<a href="#content" id="btnStart" class="boton bTri btn1">INICIAR</a>
+						<a href="" id="btnContact" class="boton bTri btn2">MODELOS</a>
+					</div>	
 				</div>
 			</div>
-			<div class="contLogoMenus">
-				<div class="menus">
-					<a href="#"class="boton bTri btn3">EMPEZAR</a>
-					<a href="" class="boton bTri btn3">MODELOS</a>
-					<a href="#"class="boton bTri btn3">EMPEZAR</a>
-					<a href="" class="boton bTri btn3">MODELOS</a>
+		</header>
+	<!-- /header -->
+		<content id="content">
+			<div id="contentHeader">
+				<div class="contentMenu">
+					<div class="contLogoH">
+						<div class="logoH">
+							<img class="logoImgH" src="image/logohorizontal.png" alt="Logo de Escenika Horizontal">
+						</div>
+					</div>
+					<div class="contLogoMenus">
+						<div class="menus">
+							<a href="#"class="boton bTri btn3">ARQUITECTURA</a>
+							<a href="" class="boton bTri btn3">DEPARTAMENTOS</a>
+							<a href="#"class="boton bTri btn3">ESTILO DE VIDA</a>
+							<a href="" class="boton bTri btn3">UBICACIÓN</a>
+						</div>
+					</div>
+				</div>
+				<div class="contPano">
+					<div class="contMjeAbout">
+						<div class="positionContMje">
+							<p class="txtAbout tittleNosotros">
+								ACERCA DE 
+								<br>
+								<strong>ESCENIKA</strong>
+							</p>
+							<p class="txtAbout contentNosotros">
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+								quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+								consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+								cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+								proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+							</p>
+						</div>
+					</div>
 				</div>
 			</div>
-		</div>
-		<div class="contPano">
+			<div id="contentProyect">
+				<div class="contenCaract">
+
+						<div class="contentDescription">
+							<div class="banderaText">
+								<div class="backIconLogoC">
+									
+								</div>
+								<div class="iconLogoC">
+									<!--
+									<img class="iconoDesc" src="icons/edificio.png">-->
+								</div>
+								<h2 class="tDesc">PERFECTO</h2>
+								<h3 class="stDesc">DISEÑO Y ACABADO</h3>							
+								<hr class="lineaBandera">
+							<p class="txtDesc">
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
+							</p>
+							</div>
+							<div class="bandera">
+							</div>
+						</div>	
+										<div class="contentDescription">
+							<div class="banderaText">
+								<div class="backIconLogoC">
+									
+								</div>
+								<div class="iconLogoC">
+									<!--
+									<img class="iconoDesc" src="icons/edificio.png">-->
+								</div>
+								<h2 class="tDesc">PERFECTO</h2>
+								<h3 class="stDesc">DISEÑO Y ACABADO</h3>							
+								<hr class="lineaBandera">
+							<p class="txtDesc">
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
+							</p>
+							</div>
+							<div class="bandera">
+							</div>
+						</div>	
+												<div class="contentDescription">
+							<div class="banderaText">
+								<div class="backIconLogoC">
+									
+								</div>
+								<div class="iconLogoC">
+									<!--
+									<img class="iconoDesc" src="icons/edificio.png">-->
+								</div>
+								<h2 class="tDesc">PERFECTO</h2>
+								<h3 class="stDesc">DISEÑO Y ACABADO</h3>							
+								<hr class="lineaBandera">
+							<p class="txtDesc">
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
+							</p>
+							</div>
+							<div class="bandera">
+							</div>
+						</div>	
+												<div class="contentDescription">
+							<div class="banderaText">
+								<div class="backIconLogoC">
+									
+								</div>
+								<div class="iconLogoC">
+									<!--
+									<img class="iconoDesc" src="icons/edificio.png">-->
+								</div>
+								<h2 class="tDesc">PERFECTO</h2>
+								<h3 class="stDesc">DISEÑO Y ACABADO</h3>							
+								<hr class="lineaBandera">
+							<p class="txtDesc">
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
+							</p>
+							</div>
+							<div class="bandera">
+							</div>
+						</div>	
+				</div>
+			</div>
+		</content>
+		<footer>
 			
-			
-		</div>
-	</div>
-</content>
-<footer>
-	
-</footer>
-</body>
+		</footer>
+	</body>
 </html>
