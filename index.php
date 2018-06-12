8a8ff2ec2e7c72a6d59963019411884b0fa49e43
chrispumita <christian.floppy@gmail.com>
2018-06-12 18:06:41 -0500

    Incorpored JS
    Java Script ACESS

--- index.php
+++ index.php
@@ -7,28 +7,114 @@
 	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
 	<link rel="stylesheet" href="style/style.css">
 
-	<style>
 
-/* Fading animation */
-.fade {
-  -webkit-animation-name: fade;
-  -webkit-animation-duration: 2.5s;
-  animation-name: fade;
-  animation-duration: 1.5s;
+<style>
+
+/* The ModalPhotoVirtual (background) */
+.modalPhotoVirtual, .modalPhotoVirtual2 {
+    display: none;
+    position: fixed;
+    z-index: 1;
+    padding-top: 1%;
+    /* left: 0; */
+    top: 0;
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+    /* background-color: rgb(0,0,0); */
+    background-color: rgba(0, 0, 0, 0.58);
 }
 
-@-webkit-keyframes fade {
-  from {opacity: .9} 
-  to {opacity: 1}
+/* ModalPhotoVirtual Content */
+.modalPhotoVirtual-content, .modalPhotoVirtual-content2 {
+    background-color: var(--grey);
+    margin: auto;
+    padding: 20px;
+    /* border: 1px solid #9E9E9E; */
+    width: 90%;
+    height: 90%;
 }
 
-@keyframes fade {
-  from {opacity: .4} 
-  to {opacity: 1}
+/* The CloseBtnPhoto Button */
+.closeBtnPhoto, .closeBtnPhoto2 {
+    color: #aaaaaa;
+    float: right;
+    font-size: 28px;
+    font-weight: bold;
+}
+
+.closeBtnPhoto:hover,
+.closeBtnPhoto:focus,
+.closeBtnPhoto2:hover,
+.closeBtnPhoto2:focus {
+    color: #000;
+    text-decoration: none;
+    cursor: pointer;
+}
+</style>
+
+<style>
+
+/* Slides */
+.mySalesManSlider {
+  display: none;
+  padding: 80px;
+  text-align: center;
+}
+
+/* sigSalesMan & prevSalesManious buttons */
+.prevSalesMan, .sigSalesMan {
+  cursor: pointer;
+  position: absolute;
+  top: 50%;
+  width: auto;
+  margin-top: -30px;
+  padding: 16px;
+  color: #888;
+  font-weight: bold;
+  font-size: 20px;
+  border-radius: 0 3px 3px 0;
+  user-select: none;
+}
+
+/* Position the "sigSalesMan button" to the right */
+.sigSalesMan {
+  position: absolute;
+  right: 0;
+  border-radius: 3px 0 0 3px;
+}
+
+/* On hover, add a black background color with a little bit see-through */
+.prevSalesMan:hover, .sigSalesMan:hover {
+  background-color: rgba(0,0,0,0.8);
+  color: white;
+}
+
+/* The circleDot/bullet/indicator container */
+.circleDot-container {
+    text-align: center;
+    padding: 20px;
+    background: #ddd;
+}
+
+/* The circleDots/bullets/indicators */
+.circleDot {
+  cursor: pointer;
+  height: 15px;
+  width: 15px;
+  margin: 0 2px;
+  background-color: #bbb;
+  border-radius: 50%;
+  display: inline-block;
+  transition: background-color 0.6s ease;
+}
+
+/* Add a background color to the activeSelectesSalesMan circleDot/circle */
+.activeSelectesSalesMan, .circleDot:hover {
+  background-color: #717171;
 }
 
 </style>
-
 
 
 </head>
@@ -240,7 +326,7 @@
 									</div>
 									<div>
 										<br>
-										<a href="#content" id="btnStart" class="boton bTri btn1">RECORRIDO FOTOGRAFICO</a>
+										<a href="#contentDeptos" id="btnVirtual2" class="boton bTri btn1">RECORRIDO FOTOGRAFICO</a>
 										
 									</div>
 										
@@ -249,10 +335,6 @@
 					    </div>
 					  </div>
 					</div>
-
-
-
-
 					<div class="containerBoxCaract">
 					  <div class="card">
 					    <div class="front boxFront">
@@ -264,7 +346,7 @@
 										DEPARTAMENTO
 									</div>
 									<div class="tittle">
-										TIPO A MODELO CMYK
+										TIPO A MODELO RGB
 									</div>
 									<hr class="lineaBandera">
 										<p class="txtDesc txtBoxFormat">
@@ -276,16 +358,16 @@
 							</div>
 							<div class="iconsDescDepto">
 								<div class="txtDataDepto">
-									<img class="iconDepto" src="icons/rule.png" alt=""> 95 m2
+									<img class="iconDepto" src="icons/rule.png" alt=""> 65 m2
 								</div>
 								<div class="txtDataDepto">
-									<img class="iconDepto" src="icons/bed.png" alt=""> 3
+									<img class="iconDepto" src="icons/bed.png" alt=""> 2
 								</div>
 								<div class="txtDataDepto">
-									<img class="iconDepto" src="icons/car.png" alt=""> 2
+									<img class="iconDepto" src="icons/car.png" alt=""> 1
 								</div>
 								<div class="txtDataDepto">
-									<img class="iconDepto" src="icons/ducha.png" alt=""> 2
+									<img class="iconDepto" src="icons/ducha.png" alt=""> 1 1/2
 								</div>
 							</div>
 					    </div>
@@ -299,10 +381,9 @@
 								<div class="listPlano">
 									<ul>
 										<li>Modelo de 65 m2</li>
-										<li>3 recamaras</li>
-										<li>2 baños completos</li>
-										<li>Family room</li>
-										<li>2 cajones de estacionamiento</li>
+										<li>2 recamaras</li>
+										<li>1 baños completos</li>
+										<li>1 cajón de estacionamiento</li>
 									</ul>
 								</div>
 								<div class="tittlePlano">
@@ -314,8 +395,7 @@
 									</div>
 									<div>
 										<br>
-										<a href="#content" id="btnStart" class="boton bTri btn1">RECORRIDO FOTOGRAFICO</a>
-										
+										<a href="#contentDeptos" id="btnVirtual" class="boton bTri btn1">RECORRIDO FOTOGRAFICO</a>
 									</div>
 										
 								</div>
@@ -323,9 +403,6 @@
 					    </div>
 					  </div>
 					</div>
-
-
-
 				</div>
 			</div>
 			<div id="contentEstilo">
@@ -362,23 +439,25 @@
 						</div>
 					</div>
 					<div class="ContimagePinMAP">
-						<img class="imgMap" src="image/mapPin.png">
-						<div class="contPinBoxes1">
-							<img class="imgPin" src="icons/pin.png">
-							<div class="boxPin">
-								SALON DE USOS MUKTIMPLES
+						<div class="contenedorMapaScroll">
+							<img class="imgMap" src="image/mapPin.png">
+							<div class="contPinBoxes1">
+								<img class="imgPin" src="icons/pin.png">
+								<div class="boxPin">
+									SALON DE USOS MUKTIMPLES
+								</div>
 							</div>
-						</div>
-						<div class="contPinBoxes2">
-							<img class="imgPin" src="icons/pin.png">
-							<div class="boxPin">
-								AREAS VERDES
+							<div class="contPinBoxes2">
+								<img class="imgPin" src="icons/pin.png">
+								<div class="boxPin">
+									AREAS VERDES
+								</div>
 							</div>
-						</div>
-						<div class="contPinBoxes3">
-							<img class="imgPin" src="icons/pin.png">
-							<div class="boxPin">
-								ESTACIONAMIENTO
+							<div class="contPinBoxes3">
+								<img class="imgPin" src="icons/pin.png">
+								<div class="boxPin">
+									ESTACIONAMIENTO
+								</div>
 							</div>
 						</div>
 					</div>
@@ -399,95 +478,215 @@
 							</div>
 						</div>
 					</div>
+
+				<!-- 	SLIDERS DE VENDEDORES	-->
 					<div class="contentAsesores">
-						<div class="moveAgente">
-							&#x25C0;
-						</div>
-						<div class="contAgentePerson">
-							<!-- 	AGENTE 1 DATA -->
-							<div class="boxFrontAgent zoom">
-								<div class="boxBackIMG2">
-									<img class="imagenAjustada" src="image/vendedor2.png">
-								</div>
-								<div class="boxontactoAgente">
-									<div class="txtContactoAgente">
-										<div class="nameAgente">
-											LAURA HERNÁNDEZ
+						<div class="mySalesManSlider">
+						  <!-- 	PARTE UNO DE VENDEDORES 4 de N -->
+							<div class="contentAsesoresRandoom">
+								<div class="contAgentePerson">
+									<!-- 	AGENTE 1 DATA -->
+									<div class="boxFrontAgent zoom">
+										<div class="boxBackIMG2">
+											<img class="imagenAjustada" src="image/vendedor2.png">
 										</div>
-										<hr class="lineaBandera">
-											<p class="txtDesc txtBoxFormat">
-												lhernandez@escenika.com.mx
-											</p>
-											<a href="#contentProyect" class="boton bTri btn1 btnContactAgente">CONTACTAR</a>
+										<div class="boxontactoAgente">
+											<div class="txtContactoAgente">
+												<div class="nameAgente">
+													JOSE CARLOS
+												</div>
+												<hr class="lineaBandera">
+													<p class="txtDesc txtBoxFormat">
+														lhernandez@escenika.com.mx
+													</p>
+													<a href="#contentProyect" class="boton bTri btn1 btnContactAgente">CONTACTAR</a>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="contAgentePerson">
+																<!-- 	AGENTE 2 DATA -->
+									<div class="boxFrontAgent zoom">
+										<div class="boxBackIMG2">
+											<img class="imagenAjustada" src="image/vendedor2.png">
+										</div>
+										<div class="boxontactoAgente">
+											<div class="txtContactoAgente">
+												<div class="nameAgente">
+													LETICIA SALAS
+												</div>
+												<hr class="lineaBandera">
+													<p class="txtDesc txtBoxFormat">
+														lhernandez@escenika.com.mx
+													</p>
+													<a href="#contentProyect" class="boton bTri btn1 btnContactAgente">CONTACTAR</a>
+											</div>
+										</div>
+									</div>
+								</div>
+
+								<div class="contAgentePerson">
+																<!-- 	AGENTE 3 DATA -->
+									<div class="boxFrontAgent zoom">
+										<div class="boxBackIMG2">
+											<img class="imagenAjustada" src="image/vendedor2.png">
+										</div>
+										<div class="boxontactoAgente">
+											<div class="txtContactoAgente">
+												<div class="nameAgente">
+													CLAUDIA ESPONOZA
+												</div>
+												<hr class="lineaBandera">
+													<p class="txtDesc txtBoxFormat">
+														lhernandez@escenika.com.mx
+													</p>
+													<a href="#contentProyect" class="boton bTri btn1 btnContactAgente">CONTACTAR</a>
+											</div>
+										</div>
+									</div>
+								</div>
+
+								<div class="contAgentePerson">
+																<!-- 	AGENTE 4 DATA -->
+									<div class="boxFrontAgent zoom">
+										<div class="boxBackIMG2">
+											<img class="imagenAjustada" src="image/vendedor2.png">
+										</div>
+										<div class="boxontactoAgente">
+											<div class="txtContactoAgente">
+												<div class="nameAgente">
+													KAREN MEJIA
+												</div>
+												<hr class="lineaBandera">
+													<p class="txtDesc txtBoxFormat">
+														lhernandez@escenika.com.mx
+													</p>
+													<a href="#contentProyect" class="boton bTri btn1 btnContactAgente">CONTACTAR</a>
+											</div>
+										</div>
 									</div>
 								</div>
 							</div>
+						  <!-- 	PARTE UNO DE VENDEDORES 4 de N -->
 						</div>
-						<div class="contAgentePerson">
-														<!-- 	AGENTE 1 DATA -->
-							<div class="boxFrontAgent zoom">
-								<div class="boxBackIMG2">
-									<img class="imagenAjustada" src="image/vendedor2.png">
-								</div>
-								<div class="boxontactoAgente">
-									<div class="txtContactoAgente">
-										<div class="nameAgente">
-											LAURA HERNÁNDEZ
+
+						<div class="mySalesManSlider">
+						  <!-- 	PARTE UNO DE VENDEDORES 4 de N -->
+							<div class="contentAsesoresRandoom">
+								<div class="contAgentePerson">
+									<!-- 	AGENTE 1 DATA -->
+									<div class="boxFrontAgent zoom">
+										<div class="boxBackIMG2">
+											<img class="imagenAjustada" src="image/vendedor2.png">
 										</div>
-										<hr class="lineaBandera">
-											<p class="txtDesc txtBoxFormat">
-												lhernandez@escenika.com.mx
-											</p>
-											<a href="#contentProyect" class="boton bTri btn1 btnContactAgente">CONTACTAR</a>
+										<div class="boxontactoAgente">
+											<div class="txtContactoAgente">
+												<div class="nameAgente">
+													ALBERTO SURIÑA
+												</div>
+												<hr class="lineaBandera">
+													<p class="txtDesc txtBoxFormat">
+														lhernandez@escenika.com.mx
+													</p>
+													<a href="#contentProyect" class="boton bTri btn1 btnContactAgente">CONTACTAR</a>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="contAgentePerson">
+																<!-- 	AGENTE 2 DATA -->
+									<div class="boxFrontAgent zoom">
+										<div class="boxBackIMG2">
+											<img class="imagenAjustada" src="image/vendedor2.png">
+										</div>
+										<div class="boxontactoAgente">
+											<div class="txtContactoAgente">
+												<div class="nameAgente">
+													CARLOS DAVID
+												</div>
+												<hr class="lineaBandera">
+													<p class="txtDesc txtBoxFormat">
+														lhernandez@escenika.com.mx
+													</p>
+													<a href="#contentProyect" class="boton bTri btn1 btnContactAgente">CONTACTAR</a>
+											</div>
+										</div>
+									</div>
+								</div>
+
+								<div class="contAgentePerson">
+																<!-- 	AGENTE 3 DATA -->
+									<div class="boxFrontAgent zoom">
+										<div class="boxBackIMG2">
+											<img class="imagenAjustada" src="image/vendedor2.png">
+										</div>
+										<div class="boxontactoAgente">
+											<div class="txtContactoAgente">
+												<div class="nameAgente">
+													FRANCISCO JAVIER LOPEZ
+												</div>
+												<hr class="lineaBandera">
+													<p class="txtDesc txtBoxFormat">
+														lhernandez@escenika.com.mx
+													</p>
+													<a href="#contentProyect" class="boton bTri btn1 btnContactAgente">CONTACTAR</a>
+											</div>
+										</div>
+									</div>
+								</div>
+
+								<div class="contAgentePerson">
+																<!-- 	AGENTE 4 DATA -->
+									<div class="boxFrontAgent zoom">
+										<div class="boxBackIMG2">
+											<img class="imagenAjustada" src="image/vendedor2.png">
+										</div>
+										<div class="boxontactoAgente">
+											<div class="txtContactoAgente">
+												<div class="nameAgente">
+													LAURA HERNÁNDEZ
+												</div>
+												<hr class="lineaBandera">
+													<p class="txtDesc txtBoxFormat">
+														lhernandez@escenika.com.mx
+													</p>
+													<a href="#contentProyect" class="boton bTri btn1 btnContactAgente">CONTACTAR</a>
+											</div>
+										</div>
 									</div>
 								</div>
 							</div>
+						  <!-- 	PARTE UNO DE VENDEDORES 4 de N -->
 						</div>
-						<div class="contAgentePerson">
-														<!-- 	AGENTE 1 DATA -->
-							<div class="boxFrontAgent zoom">
-								<div class="boxBackIMG2">
-									<img class="imagenAjustada" src="image/vendedor2.png">
-								</div>
-								<div class="boxontactoAgente">
-									<div class="txtContactoAgente">
-										<div class="nameAgente">
-											LAURA HERNÁNDEZ
-										</div>
-										<hr class="lineaBandera">
-											<p class="txtDesc txtBoxFormat">
-												lhernandez@escenika.com.mx
-											</p>
-											<a href="#contentProyect" class="boton bTri btn1 btnContactAgente">CONTACTAR</a>
-									</div>
-								</div>
-							</div>
+						<div class="mySalesManSlider">
+						  <q>I love you the more in that I believe ynd for nothing else</q>
+						  <p class="author">- John Keats</p>
 						</div>
-						<div class="contAgentePerson">
-														<!-- 	AGENTE 1 DATA -->
-							<div class="boxFrontAgent zoom">
-								<div class="boxBackIMG2">
-									<img class="imagenAjustada" src="image/vendedor2.png">
-								</div>
-								<div class="boxontactoAgente">
-									<div class="txtContactoAgente">
-										<div class="nameAgente">
-											LAURA HERNÁNDEZ
-										</div>
-										<hr class="lineaBandera">
-											<p class="txtDesc txtBoxFormat">
-												lhernandez@escenika.com.mx
-											</p>
-											<a href="#contentProyect" class="boton bTri btn1 btnContactAgente">CONTACTAR</a>
-									</div>
-								</div>
-							</div>
+						<div class="mySalesManSlider">
+						  <q>I love you the more ined me for my own sake and for nothing else</q>
+						  <p class="author">- John Keats</p>
 						</div>
-						<div class="moveAgente">
-							&#x25BA;
+						<div class="mySalesManSlider">
+						  <q>I love you the more in that I believe you had liked me foor nothing else</q>
+						  <p class="author">- John Keats</p>
 						</div>
+						<div class="mySalesManSlider">
+						  <q>I love you the more in that I believe you had liked me for my own sake and for nothing else</q>
+						  <p class="author">- John Keats</p>
+						</div>
+						<div class="mySalesManSlider">
+						  <q>But man is not made for defeat. oyed but not defeated.</q>
+						  <p class="author">- Ernest Hemingway</p>
+						</div>
+						<div class="mySalesManSlider">
+						  <q>I have not failed. I've just found 10,000 ways that won't work.</q>
+						  <p class="author">- Thomas A. Edison</p>
+						</div>
+						<a class="prevSalesMan" onclick="plusSlides(-1)">❮</a>
+						<a class="sigSalesMan" onclick="plusSlides(1)">❯</a>
 					</div>
 				</div>
+
 				<div  id="locationGoo" class="contentMapa">
 					<div class="contInstruccions">
 						<p class="instructions">
@@ -551,7 +750,8 @@
 			</div>
 			<div class="autorized">
 				<p class="txtAutorixed">
-					Autorizado mediante gaceta de Gobierno del Estado de México tomo CCIV números 34 y 35 de fecha 17 y 18 de Agosto de 2017.
+					Autorizado mediante gaceta de Gobierno del Estado de México tomo CCIV números 34 y 35 de fecha 17 y 18 de Agosto de 2017. <br>
+					Aviso de Privacidad
 				</p>
 			</div>
 			<div class="contenedorArriba">
@@ -559,24 +759,46 @@
 				<a href="#" class="to-top">Ir arriba</a>
 			</div>	
 		</footer>
+<!--
+		MODALS EMPLEADOS EN PARA VISUALIZAR OBJETOS
+-->
+<!-- Trigger/Open The ModalPhotoVirtual -->
+
+
+	<!-- The ModalPhotoVirtual -->
+	<div id="myModalPhotoVirtual" class="modalPhotoVirtual">
+
+	  <!-- ModalPhotoVirtual content -->
+	  <div class="modalPhotoVirtual-content">
+	    <span class="closeBtnPhoto">&times;</span>
+	    <p class="subtittle">RECORRIDO FOTOGRÁFICO VIRTUAL - DEPARTAMENTO CMYK MOD 2</p>
+			<iframe class="recorrido-iframe" style="width:  100%;height:  90%;" src="https://my.matterport.com/show/?m=H6PfMfCeZTi&lang=es" frameborder="0" allowfullscreen>
+			</iframe>
+	  </div>
+
+	</div>
+
+		<!-- The ModalPhotoVirtual -->
+	<div id="myModalPhotoVirtual2" class="modalPhotoVirtual2">
+
+	  <!-- ModalPhotoVirtual content -->
+	  <div class="modalPhotoVirtual-content2">
+	    <span class="closeBtnPhoto2">&times;</span>
+	    <p class="subtittle">RECORRIDO FOTOGRÁFICO VIRTUAL - DEPARTAMENTO RGB MOD 1</p>
+			<iframe class="recorrido-iframe" style="width:  100%;height:  90%;" src="https://my.matterport.com/show/?m=H6PfMfCeZTi&lang=es" frameborder="0" allowfullscreen>
+			</iframe>
+	  </div>
+
+	</div>
+
+
+
  <!-- SCRIPTS -->
-		 <script>
-		var slideIndex = 0;
-		showSlides();
 
-		function showSlides() {
-		    var i;
-		    var slides = document.getElementsByClassName("mySlides");
-		    for (i = 0; i < slides.length; i++) {
-		       slides[i].style.display = "none";  
-		    }
-		    slideIndex++;
-		    if (slideIndex > slides.length) {slideIndex = 1}   
-		    slides[slideIndex-1].style.display = "block";  
-		    setTimeout(showSlides, 4000); // Change image every 2 seconds
-		}
-		</script>
+<script>
 
+</script>
 
+		  <script src="javascripts/application.js" type="text/javascript" charset="utf-8" async defer></script>
 	</body>
 </html>
---
+++ javascripts/application.js
@@ -0,0 +1,88 @@
+var slideIndex = 0;
+			showSlides();
+
+			function showSlides() {
+			    var i;
+			    var slides = document.getElementsByClassName("mySlides");
+			    for (i = 0; i < slides.length; i++) {
+			       slides[i].style.display = "none";  
+			    }
+			    slideIndex++;
+			    if (slideIndex > slides.length) {slideIndex = 1}   
+			    slides[slideIndex-1].style.display = "block";  
+			    setTimeout(showSlides, 4000); // Change image every 2 seconds
+			}
+/* ******************** */
+// Get the modalPhotoVirtual
+var modalPhotoVirtual = document.getElementById('myModalPhotoVirtual');
+
+// Get the button that opens the modalPhotoVirtual
+var btn = document.getElementById("btnVirtual");
+
+// Get the <span> element that closeBtnPhotos the modalPhotoVirtual
+var span2 = document.getElementsByClassName("closeBtnPhoto")[0];
+
+// When the user clicks the button, open the modalPhotoVirtual 
+btn.onclick = function() {
+    modalPhotoVirtual.style.display = "block";
+}
+
+// When the user clicks on <span> (x), closeBtnPhoto the modalPhotoVirtual
+span2.onclick = function() {
+    modalPhotoVirtual.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modalPhotoVirtual, closeBtnPhoto it
+window.onclick = function(event) {
+    if (event.target == modalPhotoVirtual) {
+        modalPhotoVirtual.style.display = "none";
+    }
+}
+/* ******************** */
+// Get the modalPhotoVirtual
+var modalPhotoVirtual2 = document.getElementById('myModalPhotoVirtual2');
+
+// Get the button that opens the modalPhotoVirtual2
+var btn2 = document.getElementById("btnVirtual2");
+
+// Get the <span> element that closeBtnPhotos the modalPhotoVirtual2
+var span = document.getElementsByClassName("closeBtnPhoto2")[0];
+
+// When the user clicks the button, open the modalPhotoVirtual2 
+btn2.onclick = function() {
+    modalPhotoVirtual2.style.display = "block";
+}
+
+// When the user clicks on <span> (x), closeBtnPhoto the modalPhotoVirtual2
+span.onclick = function() {
+    modalPhotoVirtual2.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modalPhotoVirtual2, closeBtnPhoto it
+window.onclick = function(event) {
+    if (event.target == modalPhotoVirtual2) {
+        modalPhotoVirtual2.style.display = "none";
+    }
+}
+/* ******************** */
+var slideIndexPhotoSales = 1;
+showSlidesPhotoSales(slideIndexPhotoSales);
+
+function plusSlides(n) {
+  showSlidesPhotoSales(slideIndexPhotoSales += n);
+}
+
+function currentSlide(n) {
+  showSlidesPhotoSales(slideIndexPhotoSales = n);
+}
+
+function showSlidesPhotoSales(n) {
+  var i;
+  var slidesPhoto = document.getElementsByClassName("mySalesManSlider");
+  if (n > slidesPhoto.length) {slideIndexPhotoSales = 1}    
+  if (n < 1) {slideIndexPhotoSales = slidesPhoto.length}
+  for (i = 0; i < slidesPhoto.length; i++) {
+      slidesPhoto[i].style.display = "none";  
+  }
+  slidesPhoto[slideIndexPhotoSales-1].style.display = "block";
+}
